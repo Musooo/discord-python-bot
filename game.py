257fa8862cc8c6c895302a5d9c284ac86c3f1cba
Musooo <nicoloblack2@gmail.com>
2024-05-02 16:08:58 +0200

    bug fix

--- game.py
+++ game.py
@@ -10,10 +10,10 @@ class Game:
     def result(self): # game logic implementation that return the result of the game
         if self.userC==self.compC:
             return "Tie"
-        elif self.userC == 1 and (self.compC==3 or self.compC==4) or self.userC == 2 and (self.compC==1 or self.compC==5) or self.userC == 3 and (self.compC==2 or self.compC==3) or self.userC == 4 and (self.compC==2 or self.compC==3) or self.userC == 5 and (self.compC==1 or self.compC==4):
+        elif self.userC == 1 and (self.compC==3 or self.compC==4) or self.userC == 2 and (self.compC==1 or self.compC==5) or self.userC == 3 and (self.compC==2 or self.compC==5) or self.userC == 4 and (self.compC==2 or self.compC==3) or self.userC == 5 and (self.compC==1 or self.compC==4):
             return "you lose"
         else :
             return "you won"
     
     def printC(self): # return the choices of the user and the bot
-        return f"you choose: {self.choiceL[self.userC]}\nthe bot choose: {self.choiceL[self.compC]}"
+        return f"you choose: {self.choiceL[self.userC-1]}\nthe bot choose: {self.choiceL[self.compC-1]}"
